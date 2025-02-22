e52a952027c35098ccbd2733bad3a55a654fd755
ganesh <ganeshgreat08@gmail.com>
2025-02-22 22:14:00 +0530

    added new

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
 app.use("/",formRouter)
 // app.listen(3000,()=>{
 //     console.log(('Server is running on port 3000',`http://localhost:3000`))
